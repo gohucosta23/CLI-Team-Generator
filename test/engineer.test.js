6f5eb6de6fef33fc669e1f38675b6d03574572c4
Hugo_Costa <gohucosta23@hotmail.com>
2020-04-17 11:24:25 -0400

    Added methods and subclasses needed

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,7 +1,7 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
-  const testValue = "GitHubUser";
+  const testValue = "Foo";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
@@ -14,6 +14,6 @@ test("getRole() should return \"Engineer\"", () => {
 
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new Engineer("test@test.com", 1, "Foo", testValue);
   expect(e.getGithub()).toBe(testValue);
 });
